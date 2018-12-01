beb536837d4c76b23f1409c33a2adcf5b66652fc
Mohammed Alsubhi <mjs.ksa@gmail.com>
2018-12-01 17:22:36 +0300

    reduce import line

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request, redirect, url_for, make_response, flash, session as login_session
+from flask import Flask, render_template, request, redirect
+from flask import url_for, make_response, flash, session as login_session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base, Category, Item, User
@@ -67,7 +68,7 @@ def login():
 # Gconnect route
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-     # Validate state token
+    # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -117,8 +118,9 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(json.dumps(
+            'Current user is already connected.'),
+            200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -207,7 +209,9 @@ def list_categories():
 def new_category():
     if request.method == 'POST':
         new_category = Category(
-            name=request.form['name'], desc=request.form['desc'], user_id=login_session['user_id'])
+            name=request.form['name'],
+            desc=request.form['desc'],
+            user_id=login_session['user_id'])
         session.add(new_category)
         session.commit()
         flash('Category {} successfully added'.format(new_category.name))
@@ -327,6 +331,7 @@ def delete_item(category_name, item_name):
 
 
 # // JSON endpoints
+# API V1
 
 # categories names list only
 @app.route('/api/v1/categories')
@@ -349,6 +354,8 @@ def categories_items_json():
     return json.dumps(category_dict)
 
 # single category info
+
+
 @app.route('/api/v1/categories/<int:category_id>')
 def category_json(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
@@ -356,6 +363,8 @@ def category_json(category_id):
     return json.dumps(category_json)
 
 # single category info with items
+
+
 @app.route('/api/v1/categories/<int:category_id>/items')
 def category_items_json(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
@@ -368,11 +377,12 @@ def category_items_json(category_id):
     return json.dumps(category_dict)
 
 
-# single item info 
+# single item info
 @app.route('/api/v1/categories/<int:category_id>/items/<int:item_id>')
-def item_json(category_id,item_id):
+def item_json(category_id, item_id):
     category = session.query(Category).filter_by(id=category_id).one()
-    item = session.query(Item).filter_by(category_id=category_id,id = item_id ).one()
+    item = session.query(Item).filter_by(
+        category_id=category_id, id=item_id).one()
     category_dict = [category.serialize]
     for c in range(len(category_dict)):
         items = [i.serialize for i in session.query(Item).filter_by(
@@ -380,6 +390,7 @@ def item_json(category_id,item_id):
         if items:
             category_dict[c]["Items"] = items
     return json.dumps(category_dict)
+
 
 if __name__ == '__main__':
     app.debug = True
